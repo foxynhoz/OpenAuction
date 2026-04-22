f4a770e80476b17e1441dbb725fe5774fdd40843
Daniel Foxyz <145469300+foxynhoz@users.noreply.github.com>
2026-04-22 19:32:11 -0300

    Updated Algorithm

---
+++ Assets/Scripts/Config/Lance_Atual.cs
@@ -0,0 +1,50 @@
+using System.IO;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Lance_Atual : MonoBehaviour
+{
+    public Text LanceAtual_TXT;
+    public Text EntreLance_TXT;
+
+    public float entreLance = 10f;
+    public float LanceValue = 0f;
+
+    void Start()
+    {
+        File.WriteAllText("LanceAtual.txt", LanceValue.ToString());
+    }
+
+    void Update()
+    {
+        EntreLance_TXT.text = entreLance.ToString();
+        LanceAtual_TXT.text = LanceValue.ToString();
+
+        
+
+    }
+
+    public void addLance()
+    {
+        LanceValue += entreLance;
+        File.WriteAllText("LanceAtual.txt", LanceValue.ToString());
+
+    }
+    public void subLance()
+    {
+        if (LanceValue > 0f)
+        {
+            LanceValue -= entreLance;
+            File.WriteAllText("LanceAtual.txt", LanceValue.ToString());
+            if (LanceValue < 0f)
+            {
+                LanceValue = 0f;
+            }
+        }
+    }
+
+    public void changeEntreLance()
+    {
+        entreLance = float.Parse(EntreLance_TXT.text);
+    }
+}
